91404043fd3808a0316a07a4618c9b55e3260d89
tito <twiceqbit@gmail.com>
2022-12-24 04:04:36 -0300

    feat: css cards

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export const DivStyled = styled.div`
   height: 100%;
   width: 100%;
-  div {
-    width: 100%;
-    height: 3.75rem;
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
- 
-  }
+`;
+export const DivSelectStyled = styled.div`
+  width: 100%;
+  height: 3.75rem;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
 `;
