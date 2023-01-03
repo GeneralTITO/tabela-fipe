5a177f8a73fbcc3b09d8dc4e75a4f8dfa16a655b
tito <twiceqbit@gmail.com>
2023-01-03 18:59:42 -0300

    borda

---
+++ src/pages/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const DivStyled = styled.div`
+  width: 100%;
+`;
+export const DivSelectStyled = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.87);
+  border-radius: 8px;
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  gap: 20px;
+`;
